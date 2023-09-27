042582274d71c5cc2404a7b85f523dfa216fe0f8
Freebird <bo.salinas12@gmail.com>
2023-09-26 21:59:07 -0500

    added useState for nextQuestion and answerSelection

--- client/src/components/Quiz/Quiz.js
+++ client/src/components/Quiz/Quiz.js
@@ -1,33 +1,62 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 import "./Quiz.css";
 import { QUERY_QUIZ } from '../../utils/queries';
 import { useQuery } from '@apollo/client';
-// const [currentQuestion, setCurrentQuestion] = useState(0);
-// const { question, answers, correct_answers} = questions[currentQuestion]
-// const [showResults, setShowResults] = useState(false)
 
-function Quiz() {
-    const { loading, data } = useQuery(QUERY_QUIZ);
+const Quiz = () => {
+    const { loading, data, error } = useQuery(QUERY_QUIZ);
     const quizzes = data?.quizzes || [];
-    console.log(quizzes.question)
+
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
+    if (error) {
+        console.error("Error fetching quiz data:", error);
+        return <p>Error fetching quiz data.</p>;
+    }
+
+    const quiz = quizzes[currentQuestion];
+
+    const handleNextQuestion = () => {
+        setCurrentQuestion(currentQuestion + 1);
+        setSelectedAnswer(null);
+    };
+    const handleAnswerSelection = (answerIndex) => {
+        setSelectedAnswer(answerIndex);
+    };    
+
+    
     return (
-        <div className='quizCard'>
-            {quizzes.question}
-            {quizzes.answers}
-            {quizzes.correct_answer}
-            <div className='quiz'>
-                <h2>Quiz question</h2>
+       <div className='quizCard'>
+        <div className='quiz'>
+            <div>
+                <h3>{quiz.question}</h3>
                 <ul className='quizChoices'>
-                    <li className='selectedAnswer'>Quiz Choices</li>
+                    {quiz.answers.map((answer, answerIndex) => (
+                        <li key={answerIndex} className={`quizChoice ${selectedAnswer === answerIndex ? 'selected': ''}`}
+                        onClick={() => handleAnswerSelection(answerIndex)}>
+                            {answer}
+                        </li>
+                    ))}
                 </ul>
-                <button>Next</button>
+                {currentQuestion < quizzes.length - 1 && (<button onClick={handleNextQuestion}>Next</button>)}
             </div>
-            {/* <div className='results'>
-                <h2>Results</h2>
-            </div> */}
         </div>
+        <div className='results'>
+            {currentQuestion === quizzes.length - 1 && (
+                <div>
+                    <p>Quiz is complete!</p>
+                </div>
+            )}
+        </div>
+    </div> 
     );
 };
+
 
 export default Quiz;
 
