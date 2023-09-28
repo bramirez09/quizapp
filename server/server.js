5c9cafa40a16ad308bfea56e6847af018371d9b0
Jayne Valverde <jaynevalverde@gmail.com>
2023-09-28 18:45:04 -0500

    bugs worked out finally

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path');
+const db = require('./config/connections');
+
+// import ApooloServer
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 // import typeDefs and Resolvers
 const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connections');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
-
+const PORT = process.env.PORT || 3001;
 
 // add express middleward
 app.use(express.urlencoded({ extended: false }));
